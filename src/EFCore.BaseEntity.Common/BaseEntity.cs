db41930800f742c4dea3e6f42fc0702b26a71b2a
burak türkoğlu <burak_2875@hotmail.com>
2022-04-29 10:17:22 +0300

    first commit.

---
+++ src/EFCore.BaseEntity.Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿/*
+** BurakQuartz v1.0.0 ()
+** Copyright © 2022 BurakQuartz. All rights reserved.
+*/
+using System.ComponentModel.DataAnnotations;
+namespace EFCore.BaseEntity.Common
+{
+    public class BaseEntity<T>
+    {
+        [Key]
+        public T Id { get; set; } = default!;
+    }
+}
